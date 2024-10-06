9b5f268f074225f089e1f1f7abac3b031c95e4fa
aaa <peterstonesooo@outlook>
2024-10-06 14:56:11 +0800

    ++

--- config/filesystem.php
+++ config/filesystem.php
@@ -23,11 +23,11 @@ return [
         // 更多的磁盘配置信息
         'qiniu' =>[									//完全可以自定义的名称
             'type'=>'qiniu',						//可以自定义,实际上是类名小写
-            'accessKey' =>'jpDClka0AFROaejYyw-MJccYgZnkdbPZKo-CqN6W',		//七牛云的配置,accessKey
-            'secretKey'=>'Zq4jd7lAorIuEVEMXkpqXfEvgCEE2c_bJqPCYYFG',//七牛云的配置,secretKey
-            'bucket'=>'store142',					//七牛云的配置,bucket空间名
+            'accessKey' =>'HOWbLrlxYAxuPx0WCHdjLyS5qYkgxUqyFbzqeuAj',		//七牛云的配置,accessKey
+            'secretKey'=>'LUafaV40f27dIlYk18wTgT8pw0J4ckiA5k4AjjUl',//七牛云的配置,secretKey
+            'bucket'=>'oooimg',					//七牛云的配置,bucket空间名
             //'domain'=>'s2dgpwe6t.hn-bkt.clouddn.com'					//七牛云的配置,domain,域名
-            'domain'=>'gf.yjtno.com',				//七牛云的配置,domain,域名
+            'domain'=>'skto12azc.sabkt.gdipper.com',				//七牛云的配置,domain,域名
         ],
     ],
 ];
