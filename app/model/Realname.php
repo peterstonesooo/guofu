0853241c015dd5bdefa4bde03fbf9ed4868ea430
aaa <peterstonesooo@outlook>
2024-11-06 21:16:16 +0800

    +

--- app/model/Realname.php
+++ app/model/Realname.php
@@ -25,7 +25,7 @@ class Realname extends Model
                 User::where('id',$realname['user_id'])->update(['is_realname'=>1,'realname'=>$realname['realname'],'ic_number'=>$realname['ic_number']]);
                 User::changeInc($user['up_user_id'], 5,'integral',24,$user['id'],2,'直推实名赠送积分',0,4,'ZS');            
             } 
-            if($user['update_realname'] == 1){
+            if($status == 1 && $user['update_realname'] == 1){
                 User::where('id',$realname['user_id'])->update(['update_realname'=>0,'is_realname=>1']);
             }
             Db::commit();
