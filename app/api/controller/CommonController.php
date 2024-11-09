3a3d0d4f79c46286dc1a5f7d96c71691b7743636
aaa <peterstonesooo@outlook>
2024-11-10 00:49:12 +0800

    ++

--- app/api/controller/CommonController.php
+++ app/api/controller/CommonController.php
@@ -110,7 +110,7 @@ class CommonController extends BaseController
 
     public function register()
     {
-        return out(null, 10001, '正在维护中，请稍后再试');
+        //return out(null, 10001, '正在维护中，请稍后再试');
         $req = $this->validate(request(), [
             //'region|区号' => 'require',
             'phone|手机号' => 'require|alphaNum|length:11',
