da04017346ce02ccd2fccaf48bab9243fc2cba64
aaa <peterstonesooo@outlook>
2024-11-12 00:49:07 +0800

    ++

--- app/model/UserRelation.php
+++ app/model/UserRelation.php
@@ -3,7 +3,7 @@
 namespace app\model;
 
 use think\Model;
-
+use think\facade\Db;
 class UserRelation extends Model
 {
     public function getLevelTextAttr($value, $data)
@@ -37,44 +37,78 @@ class UserRelation extends Model
         return $this->belongsTo(User::class, 'sub_user_id')->field('id,realname,phone');
     }
 
-    public static function  rankList($timeStr='today'){
-        $timeStr =date('Y-m-d',strtotime($timeStr));
-        //$times = strtotime($timeStr);
-        //$timee = $times + 60*60*24;
+    public static function rankList($timeStr='yesterday') {
+        $timeStr = date('Y-m-d', strtotime($timeStr));
+        $startTime = $timeStr . ' 00:00:00';
+        $endTime = $timeStr . ' 23:59:59';
         $reward = config('map.rank_reward');
-/*         $relation = UserRelation::alias('r')
-        ->field(['count(r.sub_user_id) as team_num', 'r.user_id'])
-        ->join('user u', 'u.id = r.sub_user_id')
-        ->whereBetween('u.auth_time',[$times,$timee])
-        ->where('r.user_id', '<>', '468992')
-        ->group('r.user_id')->order('team_num', 'desc')
-        ->limit(100)->select()->toArray(); */
 
-        //$relation= "select count(r.sub_user_id) team_count,r.user_id from mp_user_relation r INNER JOIN mp_order o on r.sub_user_id = o.user_id where o.created_at BETWEEN '2024-04-23 00:00:00' and '2024-04-23 23:59:59'  group by r.user_id order by team_count desc limit 100";
+
+        $activeRank = Db::name('active_rank')->order('num','desc')->select();
+        $data= [];
+
+        foreach ($activeRank as $k => $v) {
+            $user = User::where('phone', $v['phone'])->find();
+            if (!$user) {
+                throw new \Exception('用户不存在');
+            }
+
+            $data[] = [
+                'phone'=>substr_replace($v['phone'], '****', 3, 4),
+                'num'=>$v['num'],
+                'realname'=>self::maskName($user['realname']),
+                'sort'=>$k+1,
+                'reward'=>$reward[$k+1]
+            ];
+        }
+        // 需要排除的用户ID列表
+        $excludeUsers = [
+            2105152,2105228,2105220,2105224,2105232,2105231,2105234,2105226,2105229,2105227,2105221,2105225,2105223,2105230
+        ];
+    
         $relation = UserRelation::alias('r')
-        ->field(['count(r.sub_user_id) as team_num', 'r.user_id'])
-        ->join('mp_order o', 'o.user_id = r.sub_user_id')
-        ->whereBetween('o.created_at',["$timeStr 00:00:00","$timeStr 23:59:59"])
-        ->where('o.status',2)
-        ->where('o.project_group_id',8)
-        ->where('r.user_id', '<>', '468992')
-        ->group('r.user_id')->order('team_num', 'desc')
-        ->limit(100)->select()->toArray(); 
-      
+            ->field(['count(r.sub_user_id) as team_num', 'r.user_id'])
+            ->join('mp_realname n', 'r.sub_user_id = n.user_id')
+            ->whereBetween('n.audit_time', [$startTime, $endTime])
+            ->whereNotIn('r.user_id', $excludeUsers)
+            ->group('r.user_id')
+            ->order('team_num', 'desc')
+            ->limit(6)
+            ->select()
+            ->toArray();
+    
         foreach ($relation as $k => &$v) {
-            $user = User::where('id',$v['user_id'])->find();
-            $v['phone'] = $user['phone'];
-            $v['realname'] = $user['realname'];
-            $v['phone'] = substr_replace($v['phone'],'****', 3, 4);
-            $v['sort'] = $k+1;
-            if($k<=10){
-                $v['reward'] = $reward[$k+1];
-            }else{
+
+            
+            $user = User::where('id', $v['user_id'])->find();
+            $data[] = [
+                'phone'=>substr_replace($user['phone'], '****', 3, 4),
+                'num'=>$v['team_num'],
+                'realname'=>self::maskName($user['realname']),
+                'sort'=>$k+1+4,
+            ];
+            if ($k <= 10) {
+                $v['reward'] = $reward[$k + 1+4];
+            } else {
                 $v['reward'] = 20;
             }
         }
-        
-        return $relation;
+    
+        return $data;
+    }
 
+    public static function maskName($realname) {
+        if (empty($realname)) {
+            return '';
+        }
+        
+        $len = mb_strlen($realname);
+        if ($len <= 2) {
+            // 两个字及以下的名字，只显示姓和星号
+            return mb_substr($realname, 0, 1) . '*';
+        }
+        
+        // 超过两个字的名字，中间用星号替代
+        return mb_substr($realname, 0, 1) . '*' . mb_substr($realname, 2);
     }
 }
