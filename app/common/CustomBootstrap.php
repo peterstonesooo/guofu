87064ad577859ffdcce362090234760fc4da4247
aaa <peterstonesooo@outlook>
2024-11-11 11:22:32 +0800

    ++

--- app/common/CustomBootstrap.php
+++ app/common/CustomBootstrap.php
@@ -12,18 +12,19 @@ class CustomBootstrap extends Bootstrap
      */
     public function render()
     {
+        $currentUrl = $this->url($this->currentPage());
+        $perPageSelect = '';
+        if($this->options['path']=="/admin/Capital/withdrawList.html"){
+            $perPageOptions = [10, 20, 30, 50, 100];
+            $perPageSelect = '<li><select class="form-control" style="display:inline;width:100px" onchange="location.href=this.value;">';
+            foreach ($perPageOptions as $option) {
+                $url = $currentUrl . '&per_page=' . $option;
+                $perPageSelect .= '<option value="' . $url . '"  url="'.$url.'">' . $option . '</option>';
+            }
+            $perPageSelect .= '</select></li>';
+        }
         if ($this->hasPages()) {
-                $currentUrl = $this->url($this->currentPage());
-                $perPageSelect = '';
-                if($this->options['path']=="/admin/Capital/withdrawList.html"){
-                    $perPageOptions = [10, 20, 30, 50, 100];
-                    $perPageSelect = '<li><select class="form-control" style="display:inline;width:100px" onchange="location.href=this.value;">';
-                    foreach ($perPageOptions as $option) {
-                        $url = $currentUrl . '&per_page=' . $option;
-                        $perPageSelect .= '<option value="' . $url . '"  url="'.$url.'">' . $option . '</option>';
-                    }
-                    $perPageSelect .= '</select></li>';
-                }
+
             if ($this->simple) {
                 return sprintf(
                     '<ul class="pager">%s %s</ul>',
@@ -42,6 +43,14 @@ class CustomBootstrap extends Bootstrap
 
                 );
             }
+        }else{
+            return sprintf(
+                '<ul class="pagination">%s %s</ul>',
+                "<li><span>共".$this->total()."条 ".$this->listRows()."/页</span><li> <li>",
+                //增加自定义每页条数的下拉框10,20,30,50,100
+                $perPageSelect
+
+            );
         }
     }   
 }
