4cbbb45d8cf131af210c64fa691e9fe46adf9802
aaa <peterstonesooo@outlook>
2024-11-17 16:54:45 +0800

    +

--- app/api/controller/OrderController.php
+++ app/api/controller/OrderController.php
@@ -60,7 +60,13 @@ class OrderController extends AuthController
         if($req['project_id']==1 || $req['project_id']==2 || $req['project_id']==5 || $req['project_id']==6 ){
             $order = Order::where('user_id', $user['id'])->where('project_id', $req['project_id'])->whereIn('status', [1,2])->find();
             if($order){
-                return out(null, 10001, '已有的产品未结束不能购买');
+                return out(null, 10001, '已有的产品未结束不能重复购买');
+            }
+        }
+        if($project['project_group_id']==2){
+            $order = Order::where('user_id', $user['id'])->where('project_group_id', 2)->whereIn('status', [1,2])->find();
+            if($order){
+                return out(null, 10001, '已有的产品未结束不能重复购买');
             }
         }
 
