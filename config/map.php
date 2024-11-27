b94a4d3cd3a06ae1bc07581280d57b6a7793a07e
aaa <peterstonesooo@outlook>
2024-11-27 19:33:48 +0800

    +++

--- config/map.php
+++ config/map.php
@@ -72,9 +72,8 @@ return array(
             // 6 => '驰援甘肃',
         ],
         'groupName' => [
-             ['id'=>1,'name'=>'民生帮扶计划','type'=>0,],
-             ['id'=>2,'name'=>'就业补助一期','type'=>0,],
-
+            ['id'=>2,'name'=>'就业补助一期','type'=>0,],
+            ['id'=>1,'name'=>'民生帮扶计划','type'=>0,],
         ],
         'project_house'=>[
             45=>38,
