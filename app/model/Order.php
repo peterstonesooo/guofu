7443bc782b35ba6ab508e52afda4cf5fb04b0666
aaa <peterstonesooo@outlook>
2025-05-09 20:27:44 +0800

    更新订单模型，调整项目组ID判断条件以支持更多项目组

--- app/model/Order.php
+++ app/model/Order.php
@@ -245,12 +245,14 @@ class Order extends Model
                 User::changeInc($order['user_id'],$order['allow_withdraw_money'],'allow_withdraw_money',6,$order['id'],8,$text.'提现额度');
             }
         }
-        if($order['project_group_id']==13){
+        if(in_array($order['project_group_id'],[13,16])){
             $text = $order['project_name'].'-';
             if($order['sum_amount']>0){
                 User::changeInc($order['user_id'],$order['sum_amount'],'large_subsidy',6,$order['id'],7,$text.'民生补助资金');
             }
         }
+
+
         //if ($project['project_group_id'] == 1) {
             $period = $project['period']+1;
             $end_time = strtotime("+{$period} day", strtotime(date('Y-m-d')));
