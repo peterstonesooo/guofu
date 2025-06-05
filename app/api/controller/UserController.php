80637560f4b1bf4cb7f02c511ccd1473f74e09ef
aaa <peterstonesooo@outlook>
2025-06-05 19:04:10 +0800

    更新用户控制器，扩展日志类型验证规则，新增类型 10 和 11

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -849,12 +849,12 @@ class UserController extends AuthController
         $user = $this->user;
         $req = $this->validate(request(), [
             'type' => 'require|number',
-            'log_type' => 'require|number|in:0,1,2,3,4,5,6,7,8,9',
+            'log_type' => 'require|number|in:0,1,2,3,4,5,6,7,8,9,10,11',
         ]);
         $map = config('map.user_balance_log')['type_map'];
-        $log_type = [1,2,3,4,5,6,7,8,9];
+        $log_type = [1,2,3,4,5,6,7,8,9,10,11];
         if(($req['log_type'] == '' || $req['log_type']==0)){
-            $log_type = [1,2,3,4,5,6,7,8,9];
+            $log_type = [1,2,3,4,5,6,7,8,9,10,11];
         }else{
             $log_type = [$req['log_type']];
         }
