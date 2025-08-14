2a6ececf756d21a83842cc02738c3eff2c8bb405
aaa <peterstonesooo@outlook>
2025-08-14 08:42:28 +0000

    +

--- app/api/controller/TaxesController.php
+++ app/api/controller/TaxesController.php
@@ -109,6 +109,8 @@ class TaxesController extends AuthController
         $req = $this->validate(request(),[
             'money|核准金额' => 'require|float|between:30000,9999999',
             'pay_password|支付密码' => 'require|length:6,25',
+            'pay_selected'=>'require|number',//首选 1余额支付 3团队奖励
+
         ]);
         if (empty($user['pay_password'])) {
             return out(null, 10001, '请先设置支付密码');
@@ -122,10 +124,16 @@ class TaxesController extends AuthController
         }
 
         $taxesMoney = bcmul($req['money'], 0.01,2);
-        if($user['topup_balance'] < $taxesMoney){
+/*         if($user['topup_balance'] < $taxesMoney){
             return out(null, 10001,'余额不足，请充值');
 
+        } */
+        $pay_amount = $taxesMoney;
+        if ($pay_amount >  ($user['topup_balance'] + $user['team_bonus_balance'])) {
+            exit_out(null, 10090, '余额不足');
         }
+
+
         $endTime = date('Y-m-d',strtotime(date('Y-m-d', strtotime('+7 days'))));
         $taxesData = [
             'user_id' => $user['id'],
@@ -137,15 +145,47 @@ class TaxesController extends AuthController
         ];
         Db::startTrans();
         try{
-            $taxes = TaxOrder::create($taxesData);
-            User::changeInc($user['id'], -$taxesMoney,'topup_balance',40,$taxes['id'],1,'资金核准费' );
-            User::changeInc($user['id'], -$req['money'],'large_subsidy',40,$taxes['id'],7,'资金核准' );
-            User::changeInc($user['id'], $req['money'],'team_bonus_balance',40,$taxes['id'],3,'资金核准' );
+
+           $taxes = TaxOrder::create($taxesData);
+
+
+            $txtArr = [1=>'可用余额',3=>'可提余额'];
+            if($req['pay_selected']==1){
+                $field1 = 'topup_balance';
+                $field2 = 'team_bonus_balance';
+                $logType1 = 1;
+                $logType2 = 3;
+            }else{
+                $field1 = 'team_bonus_balance';
+                $field2 = 'topup_balance';
+                $logType1 = 3;
+                $logType2 = 1;
+            }
+            
+            if($user[$field1] >= $pay_amount) {
+
+                User::changeInc($user['id'],-$pay_amount,$field1,40,$taxes['id'],$logType1,$txtArr[$logType1].'-资金核准费',0,1,'OD');
+            }else{
+                if($user[$field1]>0){
+                    User::changeInc($user['id'],-$user[$field1],$field1,40,$taxes['id'],$logType1,$txtArr[$logType1].'-资金核准费',0,1,'OD');
+                } 
+                $topup_amount = bcsub($pay_amount, $user[$field1],2);
+                if($user[$field2] >= $topup_amount) {
+                    User::changeInc($user['id'],-$topup_amount,$field2,40,$taxes['id'],$logType2,$txtArr[$logType2].'-资金核准费',0,1,'OD');
+                }else{
+                    throw new \Exception('余额不足');
+                }
+            }
+
+            //User::changeInc($user['id'], -$taxesMoney,'topup_balance',40,$taxes['id'],1,'资金核准费' );
+            //User::changeInc($user['id'], -$req['money'],'large_subsidy',40,$taxes['id'],7,'资金核准' );
+            //User::changeInc($user['id'], $req['money'],'team_bonus_balance',40,$taxes['id'],3,'资金核准' );
             
             Db::commit();
             return out($taxes);
         }catch (\Exception $e){
             Db::rollback();
+            throw $e;
             return out(null, 10001,'缴纳税费失败');
         }
     }
