979579c5d781771c0379ba999b2fec305087c147
zwass <zwass@yuanmeng.com>
2025-09-08 18:29:27 +0800

    统一回收国库

--- config/console.php
+++ config/console.php
@@ -5,12 +5,12 @@
 return [
     // 指令定义
     'commands' => [
-        'curd'	=>	'app\common\command\Curd',
-        'rule'  =>  'app\common\command\Rule',
-        'checkBonus'  =>  'app\common\command\CheckBonus',
-        'activeRank'  =>  'app\common\command\ActiveRank',
-        'checkSubsidy'  =>  'app\common\command\CheckSubsidy',
-        'checkEvents'  =>  'app\common\command\CheckEvents',
+        'curd'                => 'app\common\command\Curd',
+        'rule'                => 'app\common\command\Rule',
+        'checkBonus'          => 'app\common\command\CheckBonus',
+        'activeRank'          => 'app\common\command\ActiveRank',
+        'checkSubsidy'        => 'app\common\command\CheckSubsidy',
+        'checkEvents'         => 'app\common\command\CheckEvents',
         // 'sendCashReward'  =>  'app\common\command\SendCashReward',
         // 'autoWithdrawAudit'  =>  'app\common\command\AutoWithdrawAudit',
         // 'genarateEthAdress' =>  'app\common\command\GenarateEthAdress',
@@ -21,9 +21,10 @@ return [
         // 'checkProjectRate'  =>  'app\common\command\CheckProjectRate',
         // 'checkBonusReview'  =>  'app\common\command\CheckBonusReview',
         // 'checkAuthBonus'  =>  'app\common\command\CheckAuthBonus',
-        'sysncUser'  =>'app\common\command\SysncUser',
-        'batchRecharge'  =>'app\common\command\BatchRechargeProcess',
-        'ProjectLimited'  =>'app\common\command\ProjectLimited',
-        'mettingAudit'  =>'app\common\command\MettingAudit',
+        'sysncUser'           => 'app\common\command\SysncUser',
+        'batchRecharge'       => 'app\common\command\BatchRechargeProcess',
+        'ProjectLimited'      => 'app\common\command\ProjectLimited',
+        'mettingAudit'        => 'app\common\command\MettingAudit',
+        'recoverUserBalances' => 'app\common\command\RecoverUserBalances',
     ],
 ];
