f305683657d549f625ae0494718be6f30145511b
zwass <zwass@yuanmeng.com>
2025-09-19 18:43:59 +0800

    已拨款的靠后

--- app/admin/controller/FinanceApprovalController.php
+++ app/admin/controller/FinanceApprovalController.php
@@ -54,7 +54,8 @@ class FinanceApprovalController extends AuthController
         }
 
         // 分页和排序
-        $data = $builder->orderRaw('CASE WHEN a.after_queue_code IS NOT NULL THEN a.after_queue_code ELSE a.queue_code END ASC')
+        $data = $builder->orderRaw("CASE WHEN a.status = 2 THEN 1 ELSE 0 END")
+            ->orderRaw("CASE WHEN a.after_queue_code IS NOT NULL THEN a.after_queue_code ELSE a.queue_code END ASC")
             ->order('a.id', 'desc')
             ->paginate(['list_rows' => 15, 'query' => $req]);
 
