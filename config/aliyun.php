fe260801db6522d60d4e0512828e75054ef8db89
william <william@xingfei.com>
2025-10-21 19:55:19 +0800

    实名认证 二要素对接

---
+++ config/aliyun.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'access_key_id'     => env('ALIYUN.ALIYUN_ACCESS_KEY_ID', ''),
+    'access_key_secret' => env('ALIYUN.ALIYUN_ACCESS_KEY_SECRET', ''),
+    'endpoint'          => env('ALIYUN.ALIYUN_REALNAME_ENDPOINT', 'realnamecheck.aliyuncs.com'),
+];
